9e6581a82068d73c7da678c50fb2683ee3d836b1
Sebastian Padilla <sebastianpadilla@ascbomacmini80.local>
2025-05-23 15:43:51 -0400

    add: UserList refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { UserCard } from "./components/UserCard";
+import UserList from "./components/UserList";
 
 function App() {
   const sayHello = (name: string, age: number) => {
@@ -7,38 +7,7 @@ function App() {
   };
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 bg-cyan-950 p-20 gap-10 min-h-screen">
-      <UserCard
-        name="Luna Vega"
-        age={25}
-        onClick={() => sayHello("Luna Vega", 25)}
-      />
-      <UserCard
-        name="Brendan Patel"
-        age={30}
-        onClick={() => sayHello("Brendan Patel", 30)}
-      />
-      <UserCard
-        name="Kai Chen"
-        age={24}
-        onClick={() => sayHello("Kai Chen", 24)}
-      />
-      <UserCard
-        name="Pedro Lopez"
-        age={35}
-        onClick={() => sayHello("Pedro Lopez", 35)}
-      />
-      <UserCard
-        name="Maya Patel"
-        age={25}
-        onClick={() => sayHello("Maya Patel", 25)}
-      />
-      <UserCard
-        name="Ethan Wright"
-        age={22}
-        onClick={() => sayHello("Ethan Wright", 22)}
-      />
-    </div>
+    <UserList />
   );
 }
 export default App;
---
+++ src/components/UserList.tsx
@@ -0,0 +1,37 @@
+import React, { Component, useEffect, useState } from "react";
+
+export const UserList = () => {
+  const [users, setUsers] = useState<any>();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        const data = await response.json();
+        setUsers(data);
+      } catch (error) {
+        console.error("Error fetching users:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchUsers();
+  },[]);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <ul>
+      {users.map((user: any) => (
+        <li key={user.id}>{user.name}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default UserList;
